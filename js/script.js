318cbb7fb8d0e5e7e4e96bf4cb14d11969483068
Alexpra2 <alexpra2@gmail.com>
2024-11-29 11:29:44 +0100

    versionfinal con css

--- js/script.js
+++ js/script.js
@@ -8,45 +8,59 @@
 API https://restcountries.com/v3/all)
 */
 
-
 const apiUrl = 'https://restcountries.com/v3/all';
 
 const getBandera = async () => {
     try {
-        const response = await fetch (apiUrl);
+        const response = await fetch(apiUrl);
         if (!response.ok) {
-            throw new Error('Ha surgido un error', response.status);           
+            throw new Error('Error al obtener los datos: ' + response.status);
         }
-    const data = await response.json();
-    console.log(data) 
 
+        const data = await response.json();
+        const listaPaises = document.getElementById('lista');
 
-    obtenerInformacion(data)   
-    }
-    catch(error) {
-        console.log('Error al obtener los datos');
+        // Ordenar alfabéticamente por nombre común
+        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+
+        data.forEach((pais) => {
+            const infopais = document.createElement('li');
+
+            const imagen = pais.flags[1];
+            infopais.innerHTML = `
+            <img id=imgBandera src= '${imagen}' alt='Imagen Bandera'/>
+            <p> ${pais.name.common}</p>`;
+
+            infopais.addEventListener('click', () => ventanaEmergente(pais));
+            
+            listaPaises.appendChild(infopais);
+        });
+    } 
+    
+    catch (error) {
+        console.error('Error:', error.message);
     }
 };
 
-
-
-function obtenerInformacion(paises) {
-const listaPaises = document.getElementById('lista');
-
-paises.forEach((pais) => {
+function ventanaEmergente(pais) {
+    const popup = document.getElementById('popup');
+    const paisEmergente = document.getElementById('propiedadesPais');
     
-    const infopais = document.createElement('li');
+
     const imagen = pais.flags[1];
-    infopais.innerHTML =`
-    <img id=imgBandera src= '${imagen}' alt='Imagen Bandera/>'
+    paisEmergente.innerHTML = `
+    <img src= '${imagen}' alt='Imagen Bandera'/>
     <p> ${pais.name.common}</p>
     <p>Capital: ${pais.capital[0]}</p>
-    <p>Población:${pais.population}</p>
+    <p>Población: ${pais.population}</p>
     <p>Conducen por ${pais.car.side}</p>`;
-    listaPaises.appendChild(infopais); 
 
-})
+    popup.classList.remove('oculto');
+}
 
-    
-} ;
+popupbtn.addEventListener('click', () => {
+    popup.classList.add('oculto');
+});
+
+
 getBandera();
